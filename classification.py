2b69292da4c596af8e9622f7d2033cad19bf8363
Corw <pzamyatin@gmail.com>
2016-08-24 18:35:58 +1200

    Added classification

---
+++ classification.py
@@ -0,0 +1,102 @@
+CLASSIFICATION = {
+
+    '01' :
+        {'Culture, sport and recreation' :
+            {
+            '01100' : 'Culture and arts',
+            '01200' : 'Sports',
+            '01300' : 'Other recreation and social clubs',
+            '01999' : 'Culture and recreation support and ancillary services'
+            }
+        }
+
+    # '02 Education and research' :
+    #     {
+    #     '02110' : 'Early childhood education',
+    #     '02120' : 'Primary and Secondary education',
+    #     '02200' : 'Higher education',
+    #     '02300' : 'Other education',
+    #     '02400' : 'Research',
+    #     '02999' : 'Education and research support and ancillary services',
+    #     },
+
+    # '03 Health' :
+    #     {
+    #     '03100' : 'Hospitals and rehabilitation',
+    #     '03200' : 'Nursing homes',
+    #     '03300' : 'Mental health and crisis intervention',
+    #     '03400' : 'Other health services',
+    #     '03999' : 'Health support and ancillary services'
+    #     },
+
+    # '04 Social services' :
+    #     {
+    #     '04100' : 'Social services',
+    #     '04200' : 'Emergency and relief',
+    #     '04300' : 'Income support and maintenance',
+    #     '04999' : 'Social services support and ancillary services'
+    #     },
+
+    # '05 Environment' :
+    #     {
+    #     '05100' : 'Environment',
+    #     '05200' : 'Animal protection',
+    #     '05999' : 'Environment support and ancillary services'
+    #     },
+
+    # '06 Development and housing' :
+    #     {
+    #     '06110' : 'Economic, social and community development',
+    #     '06120' : 'Tangata Whenua governance organisations',
+    #     '06200' : 'Housing',
+    #     '06300' : 'Employment and training',
+    #     '06999' : 'Development and housing support and ancillary services'
+    #     },
+
+    # '07 Law, advocacy and politics' :
+    #     {
+    #     '07100' : 'Civic and advocacy organisations',
+    #     '07200' : 'Law and legal services',
+    #     '07300' : 'Political organisations',
+    #     '07999' : 'Law, advocacy and politics support and ancillary services'
+    #     },
+
+    # '08 Grant making, fundraising and voluntarism promotion' :
+    #     {
+    #     '08100' : 'Grant making foundations',
+    #     '08210' : 'Fundraising',
+    #     '08220' : 'Voluntarism promotion',
+    #     '08999' : 'Grant making, fundraising and voluntarism promotion support and ancillary services'
+    #     },
+
+    # '09 International' :
+    #     {
+    #     '09100' : 'International activities',
+    #     '09999' : 'International support and ancillary services'
+    #     },
+
+    # '10 Religion' :
+    #     {
+    #     '10100' : 'Religious congregations and associations',
+    #     '10999' : 'Religion support and ancillary services'
+    #     },
+
+    # '11 Business and professional associations, unions' :
+    #     {
+    #     '11100' : 'Business associations',
+    #     '11200' : 'Professional associations',
+    #     '11300' : 'Labour unions',
+    #     '11999' : 'Business and professional associations, unions support and services'
+    #     },
+
+    # '99 Not elsewhere classified (residual category)' :
+    #     {
+    #     '99100' : 'Other',
+    #     '99444' : 'Don\'t know',
+    #     '99555' : 'Refused to answer',
+    #     '99777' : 'Response unidentifiable',
+    #     '99888' : 'Response outside scope',
+    #     '99999' : 'Not stated'
+    #     }
+
+    }
